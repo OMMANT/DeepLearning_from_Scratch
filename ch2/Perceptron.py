28d1551ba0c11f90e95da62d499213918640d625
OMMANT <ommant@naver.com>
2022-05-27 16:06:54 +0900

    AND numpy식으로 변경

--- ch2/Perceptron.py
+++ ch2/Perceptron.py
@@ -1,15 +1,17 @@
 import numpy as np
 
 class simple_perceptron:
-    def __init__(self, x1, x2):
-        self.x1 = x1
-        self.x2 = x2
+    def __init__(self, x: np.ndarray):
+        self.x = x
+        assert isinstance(self.x, np.ndarray), 'numpy 배열이 아님'
+        # 1차원 배열이 아닐경우 1차원 배열로
+        if np.ndim(self.x) != 1:
+            self.x = self.x.ravel()
+        self.n = len(self.x)
 
-    def AND(self):
-        x = np.array([self.x1, self.x2])
-        w = np.array([0.5, 0.5])
-        b = -0.7
-        temp = np.sum(w*x) + b
+    def AND(self, w=None, b=-0.7):        
+        w = np.array([1 / self.n] * self.n) if w is None else w
+        temp = np.sum(w*self.x) + b
         if temp <= 0:
             return 0
         else: return 1
