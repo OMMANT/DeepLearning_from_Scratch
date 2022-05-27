6c64971dbf2e2a6728500f386a35045c4d3ff250
OMMANT <ommant@naver.com>
2022-05-27 15:46:29 +0900

    AND 게이트 편향 추가된 식으로 변경

--- ch2/Perceptron.py
+++ ch2/Perceptron.py
@@ -6,9 +6,11 @@ class simple_perceptron:
         self.x2 = x2
 
     def AND(self):
-        w1, w2, theta = 0.5, 0.5, 0.7
-        temp = self.x1 * w1 + self.x2 * w2
-        if temp <= theta:
+        x = np.array([self.x1, self.x2])
+        w = np.array([0.5, 0.5])
+        b = -0.7
+        temp = np.sum(w*x) + b
+        if temp <= 0:
             return 0
         else: return 1
     
